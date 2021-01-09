e0f68efb74fe2c8d111deba78fa414afe4050931
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2021-01-09 12:43:39 +1100

    Refactor code

--- sam-lambda-step-functions/functions/stock-buyer/app.js
+++ sam-lambda-step-functions/functions/stock-buyer/app.js
@@ -7,17 +7,20 @@ function getRandomInt(max) {
 /**
  * Sample Lambda function which mocks the operation of buying a random number of shares for a stock.
  * For demonstration purposes, this Lambda function does not actually perform any  actual transactions. It simply returns a mocked result.
- * 
+ *
  * @param {Object} event - Input event to the Lambda function
  * @param {Object} context - Lambda Context runtime methods and attributes
  *
  * @returns {Object} object - Object containing details of the stock buying transaction
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
+
     // Get the price of the stock provided as input
-    stock_price = event["stock_price"]
+    stock_price = event["stock_price"];
+
     var date = new Date();
+
     // Mocked result of a stock buying transaction
     let transaction_result = {
         'id': crypto.randomBytes(16).toString("hex"), // Unique ID for the transaction
@@ -26,5 +29,6 @@ exports.lambdaHandler = async (event, context) => {
         'qty': getRandomInt(10).toString(),  // Number of shares bought / sold(We are mocking this as a random integer between 1 and 10)
         'timestamp': date.toISOString(),  // Timestamp of the when the transaction was completed
     }
-    return transaction_result
+
+    return transaction_result;
 };
